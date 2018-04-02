a3e5daae2a287d211b21a819038d0c1f7efe8b3e
Hrimfaxi <outmatch@gmail.com>
2018-04-02 14:56:47 +0800

    more alias
    Signed-off-by: Hrimfaxi <outmatch@gmail.com>

--- bashrcs/aliases.bash
+++ bashrcs/aliases.bash
@@ -1,3 +1,17 @@
 alias py='python'
 alias gitpu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 alias va='vagrant'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias hexedit='hexedit -s'
+
+alias mm='mpv --mute'
+alias sshfs='sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,compression=no'
