46c08d61d1e1f1bc8397803a8c971738280422ae
Hrimfaxi <outmatch@gmail.com>
2018-04-02 15:00:47 +0800

    added bash key binding
    Ctrl-N: nmon
    
    Signed-off-by: Hrimfaxi <outmatch@gmail.com>

--- bashrcs/main.bash
+++ bashrcs/main.bash
@@ -10,6 +10,9 @@ source ~/.bash_runtime/bashrcs/aliases.bash
 # Misc Scripts
 source ~/.bash_runtime/scripts/misc.bash
 
+# Bash key binding
+source ~/.bash_runtime/bashrcs/binding.bash
+
 # Personal
 if [ -f ~/.bash_runtime/bashrcs/personal.bash ]; then
       source ~/.bash_runtime/bashrcs/personal.bash
