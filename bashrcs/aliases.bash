0a3cf7647a136c8c1f7e7294334b695e0efd1a85
Hrimfaxi <outmatch@gmail.com>
2018-04-02 15:01:11 +0800

    added more aliases
    Signed-off-by: Hrimfaxi <outmatch@gmail.com>

--- bashrcs/aliases.bash
+++ bashrcs/aliases.bash
@@ -15,3 +15,10 @@ alias hexedit='hexedit -s'
 
 alias mm='mpv --mute'
 alias sshfs='sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,compression=no'
+
+alias ls='ls --color=auto'
+alias t='transmission-remote'
+alias tl='transmission-remote -l'
+alias tld='tl|grep -v 100%'
+alias tlu='tl|grep -v "ID     Done       Have  ETA           Up    Down  Ratio  Status       Name"|grep -v "Sum: "|sort -n -k6 -r|grep -v Stopped|grep -v Idle|head -n20'
+alias youtube-dl='youtube-dl -f "bestvideo[ext=webm]+bestaudio[ext=webm]/bestvideo+bestaudio" --write-sub --write-auto-sub --sub-lang en'
