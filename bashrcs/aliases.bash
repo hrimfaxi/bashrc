3b264cda507a4e436e13c3a92025ed9c90f41495
Hrimfaxi <outmatch@gmail.com>
2018-04-08 13:16:44 +0800

    Improve tld
    The word expands to string, with backslash-escaped
    characters replaced as specified by the ANSI C standard.
    Backslash escape sequences, if present, are decoded as follows:
    
      \\     backslash
      \'     single quote
      \"     double quote
      \n     new line
      ...
    
    Signed-off-by: Hrimfaxi <outmatch@gmail.com>

--- bashrcs/aliases.bash
+++ bashrcs/aliases.bash
@@ -19,7 +19,7 @@ alias sshfs='sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,com
 alias ls='ls --color=auto'
 alias t='transmission-remote'
 alias tl='transmission-remote -l'
-alias tld='tl|grep -v 100%'
+alias tld=$'tl | awk \' {  if ($1 == "Sum:" || ($7 != "0.0" && $7 != "None")) print $0; } \''
 alias tlu='tl|grep -v "ID     Done       Have  ETA           Up    Down  Ratio  Status       Name"|grep -v "Sum: "|sort -n -k6 -r|grep -v Stopped|grep -v Idle|head -n20'
 alias youtube-dl='youtube-dl -f "bestvideo[ext=webm]+bestaudio[ext=webm]/bestvideo+bestaudio" --write-sub --write-auto-sub --sub-lang en'
 
